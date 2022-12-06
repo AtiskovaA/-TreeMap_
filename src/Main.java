b7d7d3bf50493beb30ebe02b3188f47cdce2182e
AtiskovaA <111705918+AtiskovaA@users.noreply.github.com>
2022-12-06 15:11:58 +0300

    Исправление лямбды
    Исправление лямбды

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BiConsumer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +10,16 @@ public class Main {
         listPeople.add(new Person("Тори", "Мира Ми", 15));
         System.out.println(listPeople);
 
-        BiConsumer<Person, Person> personPersonBiConsumer = (Person p1, Person p2) ->
-                System.out.println(listPeople);
+
+        listPeople.sort ((p1, p2) -> {
+            int len1 = p1.getSurname().split(" ").length;
+            int len2 = p2.getSurname().split(" ").length;
+            if (len1 != len2) {
+                return Integer.compare(len1, len2);
+            }
+            return Integer.compare(p1.getAge(), p2.getAge());
+        });
+
+        System.out.println(listPeople);
     }
 }
